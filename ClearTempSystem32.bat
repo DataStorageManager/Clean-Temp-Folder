592f69f2fee23e591ba34180fbdf7c6c4ed90585
DataStorageManager <125272511+DataStorageManager@users.noreply.github.com>
2023-06-07 01:37:26 +0700

    Create ClearTempSystem32.bat
    Note that these commands will wipe all .tmp, .log, .dmp, .gid, .chk, .old, .bak and .wbk files in the Windows system32 directory.
    
    You can add more by editing this file.

---
+++ ClearTempSystem32.bat
@@ -0,0 +1,8 @@
+DEL /F /S /Q %windir%\System32\*.tmp
+DEL /F /S /Q %windir%\System32\*.log
+DEL /F /S /Q %windir%\System32\*.dmp
+DEL /F /S /Q %windir%\System32\*.gid
+DEL /F /S /Q %windir%\System32\*.chk
+DEL /F /S /Q %windir%\System32\*.old
+DEL /F /S /Q %windir%\System32\*.bak
+DEL /F /S /Q %windir%\System32\*.wbk  
